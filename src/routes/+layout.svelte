d7d110fcf12125bab8b3fd2d7c4f0a651d0276a8
taishi55 <105188885+taishi55@users.noreply.github.com>
2022-12-21 13:46:26 -0800

    update opengraph

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -6,6 +6,27 @@
 
 <svelte:head>
 	<meta name="author" content="Taishi Nammoto" />
+
+	<!-- Facebook Meta Tags -->
+	<meta property="og:url" content="https://jobot-liart.vercel.app/" />
+	<meta property="og:type" content="website" />
+	<meta property="og:title" content="Jobot Cloned Edition" />
+	<meta property="og:description" content="This is a cloned app" />
+	<meta
+		property="og:image"
+		content="https://www.datocms-assets.com/74544/1655148337-home-background-jobot.jpg"
+	/>
+
+	<!-- Twitter Meta Tags -->
+	<meta name="twitter:card" content="summary_large_image" />
+	<meta property="twitter:domain" content="jobot.com" />
+	<meta property="twitter:url" content="https://jobot-liart.vercel.app/" />
+	<meta name="twitter:title" content="Jobot Cloned Edition" />
+	<meta name="twitter:description" content="This is a cloned app" />
+	<meta
+		name="twitter:image"
+		content="https://www.datocms-assets.com/74544/1655148337-home-background-jobot.jpg"
+	/>
 </svelte:head>
 
 <Nav />
