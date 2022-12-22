b0c3ba508099cf1282540790d8209d45e25d998a
taishi55 <105188885+taishi55@users.noreply.github.com>
2022-12-21 21:50:01 -0800

    fix sidebar

--- src/lib/Sidebar.svelte
+++ src/lib/Sidebar.svelte
@@ -4,7 +4,9 @@
 	import { remoteOptions, sortOptions, typeOptions } from './store/filterOptions';
 </script>
 
-<div class=" bg-white sticky top-[6.8rem] rounded-lg p-5 space-y-6 shadow-xl shadow-slate-300 mb-7">
+<div
+	class=" bg-white lg:sticky lg:top-[6.8rem] rounded-lg p-5 space-y-6 shadow-xl shadow-slate-300 mb-7"
+>
 	<div class="flex items-center space-x-2">
 		<img src="/new_icon.png" class=" aspect-square object-cover w-14" alt="" />
 		<h1 class="text-lg font-bold text-sky-600 rounded-md bg-sky-100 py-1 px-3">
